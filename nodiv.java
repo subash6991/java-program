b68c4eaacf8027ff47e31e24d51c03a4762d9aeb
subash6991 <50399890+subash6991@users.noreply.github.com>
2019-05-09 12:39:30 +0530

    Add files via upload

---
+++ nodiv.java
@@ -0,0 +1,15 @@
+class nodiv
+{
+ public static void main(String args[])
+{
+   int no=532 , a,b,c;
+    a=no/100;
+    b=no%100;
+    b=b/10;
+    c=no%100;
+    c=c%10;
+System.out.println(a+" "+b+" "+c);
+}
+}
+    
+ 
